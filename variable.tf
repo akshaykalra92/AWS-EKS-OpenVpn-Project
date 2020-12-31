62bf2302f4c3c333654652c7fa031e2d610df7d3
akskalra <akshay.kalra@publicissapient.com>
2020-12-31 11:56:27 +0000

    adding ec2 and ansible for vpn

--- variable.tf
+++ variable.tf
@@ -93,29 +93,3 @@ variable "cidr" {
   default     = ""
   type        = string
 }
-
-#variable "dns_base_domain" {
-#  type        = string
-#  description = "DNS Zone name to be used from EKS Ingress."
-#}
-#variable "ingress_gateway_chart_name" {
-#  type        = string
-#  description = "Ingress Gateway Helm chart name."
-#}
-#variable "ingress_gateway_chart_repo" {
-#  type        = string
-#  description = "Ingress Gateway Helm repository name."
-#}
-#variable "ingress_gateway_chart_version" {
-#  type        = string
-#  description = "Ingress Gateway Helm chart version."
-#}
-#variable "ingress_gateway_annotations" {
-#  type        = map(string)
-#  description = "Ingress Gateway Annotations required for EKS."
-#}
-#variable "deployments_subdomains" {
-#  type        = list(string)
-#  description = "List of subdomains to be routed to Kubernetes Services."
-#}
-
